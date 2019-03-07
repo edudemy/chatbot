89193264e2e405f8cff7709e789c5fc9681caf24
Edudemy Digital Team Customer Support <edudemy@users.noreply.github.com>
2019-03-08 00:54:09 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'botUI_api.ai' });
+  res.render('index', { title: 'Luca Chatbot' });
   console.log(res.value);
 });
 
